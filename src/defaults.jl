1f81cc559a1accb63663360d48d411ef12c465bf
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-07-10 12:42:04 +0200

    Update defaults.jl to use 1.9

--- src/defaults.jl
+++ src/defaults.jl
@@ -2,7 +2,7 @@
 ### defaults for julia-version, os and branches
 
 const default_os = [ "ubuntu-latest" ]
-const default_julia = [ "~1.6.0-0", "~1.8.0-0" ]
+const default_julia = [ "~1.6.0-0", "~1.9.0-0" ]
 const default_branches = [ "<matching>", "release" ]
 
 # for each package this contains an ordered list with
@@ -11,7 +11,8 @@ const default_branches = [ "<matching>", "release" ]
 # newest entries should come first
 # if no matching version is found the doctests will run with julia 1.6
 const doctest_versions = Dict(
-    :Oscar           => [v"0.12.1-DEV" => (v"1.8", v"1.11")],
+    :Oscar           => [v"0.13.0-DEV" => (v"1.9", v"1.12"),
+                         v"0.12.1-DEV" => (v"1.8", v"1.11")],
     :Hecke           => [v"0.16.7"     => (v"1.8", v"1.9" )],
     :AbstractAlgebra => [v"0.29.5"     => (v"1.8", v"1.9" )],
     :Nemo            => [v"0.33.8"     => (v"1.8", v"1.9" )],
